76d79b683bb3b8c3028371dde14e48bb4b2ebb84
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-03-26 09:48:47 +0300

    Delete credentials folder from repo

--- credentials/mongo_uri.js
+++
@@ -1,4 +0,0 @@
-const MONGODB_URI =
-  "mongodb+srv://srknzl:PaWS1EQ7E85MHMJP@srknzl-m0-development-cluster-hgcsl.mongodb.net/serkanozel?retryWrites=true&w=majority";
-
-module.exports = { MONGODB_URI: MONGODB_URI };
--- credentials/sendgrid.js
+++
@@ -1,4 +0,0 @@
-const apiKey =
-  "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";
-
-module.exports = { apiKey: apiKey };
