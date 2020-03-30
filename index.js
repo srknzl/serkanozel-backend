99797c40a98db4cb974bad59504f7fb098932e6c
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-03-30 12:44:41 +0300

    CORS header should not be * if credentials is set

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json({
 }));
 app.use((req, res, next) => {
   res.setHeader(
-    "Access-Control-Allow-Origin", "*"
+    "Access-Control-Allow-Origin", "http://localhost:8080"
   );
   res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
