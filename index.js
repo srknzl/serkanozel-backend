492ac4575a8e284e85d03adc64749114bdb5e882
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-04-26 12:18:59 +0300

    Remove cors for poc

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.use(bodyParser.json({
 }));
 app.use((req, res, next) => {
   res.setHeader(
-    "Access-Control-Allow-Origin", "http://localhost:8080"
+    "Access-Control-Allow-Origin", "*"
   );
   res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Credentials", "false");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Content-Type,Cookie,Set-Cookie"
