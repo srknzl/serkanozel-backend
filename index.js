411ac0948efc9eefb3864706f22326d4e5534d4d
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-05-28 20:35:34 +0300

    Change cors headers

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Credentials", "false");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Content-Type,Cookie,Set-Cookie"
