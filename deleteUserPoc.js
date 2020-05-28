5f5a5880548fcdeaae226f3a3c12e597f0e5e965
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-05-28 19:03:55 +0300

    Add delete and add user poc scripts

---
+++ deleteUserPoc.js
@@ -0,0 +1,33 @@
+const axios = require("axios").default;
+const qs = require("querystring");
+
+if (process.argv.length != 3) {
+  console.error("Usage: node deleteUserPoc.js <userIRI>");
+}
+const userIRI = process.argv[2]
+
+const updateQuery = `DELETE { <http://serkanozel.me/pocUsers.ttl#poc> <http://www.w3.org/2006/vcard/ns#hasMember> <${userIRI}> .} WHERE {?s ?p ?o}`;
+const data = qs.stringify({
+  update: updateQuery
+});
+
+axios.post("http://134.122.65.239:3030/ds/update", data, {
+  auth: {
+    username: 'admin',
+    password: 'pw123'
+  },
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+})
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+
+
+
+
+
