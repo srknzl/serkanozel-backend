25427fbb621826c6a4222f10e975efc2402dcef8
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-05-28 22:18:28 +0300

    Update init script poc

--- initializePoc.js
+++ initializePoc.js
@@ -1,4 +1,4 @@
-// This script loads the initial turtle data that is at 
+// This script loads the initial turtle data that is at
 
 const axios = require("axios").default;
 const qs = require("querystring");
@@ -6,39 +6,38 @@ const { exec } = require("child_process");
 
 let ntriples;
 
-exec("./dist/apache-jena-3.15.0/bin/turtle --base=http://serkanozel.me/pocUsers.ttl pocUsers.ttl", (err, stdout, stderr) => {
-  if (err) {
-    console.log(`error: ${err.message}`);
-    process.exit(1)
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    process.exit(1)
-  }
-  ntriples = stdout;
-  const updateQuery = `INSERT {${ntriples}} WHERE {}`;
-  const data = qs.stringify({
-    update: updateQuery
-  });
-
-  axios.post("http://134.122.65.239:3030/ds/update", data, {
-    auth: {
-      username: 'admin',
-      password: 'pw123'
-    },
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+exec(
+  "turtle --base=http://serkanozel.me/pocUsers.ttl pocUsers.ttl",
+  (err, stdout, stderr) => {
+    if (err) {
+      console.log(`error: ${err.message}`);
+      process.exit(1);
     }
-  })
-    .then(res => {
-      console.log(res);
-    })
-    .catch(err => {
-      console.log(err);
-    })
-});
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      process.exit(1);
+    }
+    ntriples = stdout;
+    const updateQuery = `INSERT {${ntriples}} WHERE {}`;
+    const data = qs.stringify({
+      update: updateQuery,
+    });
 
-
-
-
-
+    axios
+      .post("http://134.122.65.239:3030/ds/update", data, {
+        auth: {
+          username: "admin",
+          password: "pw123",
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+);
