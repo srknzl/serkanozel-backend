ab5c9854374b76dc7f4cdb732a5e9841916167ee
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-06-06 15:12:21 +0300

    delete poc related endpoints do them directly with fuseki instead

--- initializePoc.js
+++ initializePoc.js
@@ -5,42 +5,6 @@ const path = require("path");
 let ntriples;
 
 exec(
-  "turtle --base=http://serkanozel.me/pocUsers.ttl pocUsers.ttl",
-  (err, stdout, stderr) => {
-    if (err) {
-      console.log(`error: ${err.message}`);
-      process.exit(1);
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      process.exit(1);
-    }
-    ntriples = stdout;
-    const updateQuery = `INSERT {${ntriples}} WHERE {}`;
-    const data = qs.stringify({
-      update: updateQuery,
-    });
-
-    axios
-      .post("http://134.122.65.239:3030/ds/update", data, {
-        auth: {
-          username: "admin",
-          password: "pw123",
-        },
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-);
-
-exec(
   `riot --rdfs="${path.join(__dirname, "graph", "poc_core.ttl")}" "${path.join(
     __dirname,
     "spec.ttl"
